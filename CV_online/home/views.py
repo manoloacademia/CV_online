e04fe60b11f597d9d08e0c36611d5da68c370b74
manoloacademia <luispablosegovia@icloud.com>
2022-07-27 12:05:02 -0300

    Templates folder moved into app folder

--- CV_online/home/views.py
+++ CV_online/home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {}
-    return render(request, 'home/home.html', context=context)
+    return render(request, 'home.html', context=context)
