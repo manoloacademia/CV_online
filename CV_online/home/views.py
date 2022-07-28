1ccc2974bbe9a0a035b50f80d8b5b68415cd7126
manoloacademia <luispablosegovia@icloud.com>
2022-07-28 10:27:30 -0300

    Added about page and extended design.

--- CV_online/home/views.py
+++ CV_online/home/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'home.html', context=context)
+
+def about(request):
+    context = {}
+    return render(request, 'about.html', context=context)
+
+def home(request):
+    context = {}
+    return render(request, 'home.html', context=context)
+
+def home(request):
+    context = {}
+    return render(request, 'home.html', context=context)
